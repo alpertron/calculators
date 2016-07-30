49527b27edced6654e934cd664715e57164c72c3
alpertron <dario@alpern.com.ar>
2016-07-29 23:19:26 -0300

    Reserve more memory (64 MB total) for polynomial factorization

--- a.bat
+++ a.bat
@@ -2,7 +2,7 @@ java -jar "C:\Program Files\Emscripten\emscripten\1.35.0\third_party\closure-com
 cmd /c emcc -Os expression.c partition.c errors.c bigint.c division.c fsquares.c fcubes.c baseconv.c karatsuba.c modmult.c sqroot.c contfrac.c --llvm-lto 1 --js-library lib.js --pre-js pre.js -s EXPORTED_FUNCTIONS="['_doWork']" -s TOTAL_MEMORY=33554432 -s NO_BROWSER=1 -s NO_FILESYSTEM=1 --closure 1 --memory-init-file 0 -o fsquaresW.js
 
 java -jar "C:\Program Files\Emscripten\emscripten\1.35.0\third_party\closure-compiler\compiler.jar" --compilation_level ADVANCED_OPTIMIZATIONS --js polyfact.js --js_output_file polfact.js
-cmd /c emcc -Os expression.c partition.c errors.c bigint.c division.c baseconv.c karatsuba.c modmult.c sqroot.c polynomial.c --llvm-lto 1 --js-library lib.js --pre-js pre.js -s EXPORTED_FUNCTIONS="['_doWork']" -s TOTAL_MEMORY=33554432 -s NO_BROWSER=1 -s NO_FILESYSTEM=1 --closure 1 --memory-init-file 0 -o polfactW.js
+cmd /c emcc -Os expression.c partition.c errors.c bigint.c division.c baseconv.c karatsuba.c modmult.c sqroot.c polynomial.c polfact.c --llvm-lto 1 --js-library lib.js --pre-js pre.js -s EXPORTED_FUNCTIONS="['_doWork']" -s TOTAL_MEMORY=67108864 -s NO_BROWSER=1 -s NO_FILESYSTEM=1 --closure 1 --memory-init-file 0 -o polfactW.js
 
 java -jar "C:\Program Files\Emscripten\emscripten\1.35.0\third_party\closure-compiler\compiler.jar" --compilation_level ADVANCED_OPTIMIZATIONS --js dislog.js --js_output_file dilog.js
 cmd /c emcc -Os expression.c partition.c errors.c bigint.c division.c baseconv.c karatsuba.c modmult.c sqroot.c factor.c dilog.c --llvm-lto 1 --js-library lib.js --pre-js pre.js -s EXPORTED_FUNCTIONS="['_doWork']" -s TOTAL_MEMORY=33554432 -s NO_BROWSER=1 -s NO_FILESYSTEM=1 --closure 1 --memory-init-file 0 -o dilogW.js
--- polyfact.js
+++ polyfact.js
@@ -26,7 +26,7 @@ function callWorker(param)
 {
   if (!worker)
   {
-  	worker = new Worker('polfactW.js?2807');
+  	worker = new Worker('polfactW.js?2907');
 	  worker.onmessage = function(e)
 	  { // First character of e.data is '1' for intermediate text
       // and it is '2' for end of calculation.
