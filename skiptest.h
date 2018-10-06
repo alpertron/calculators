8397ba8dbe50c7e0ddeafcb22d5d304ab97ba6d8
alpertron <dario@alpern.com.ar>
2018-10-06 07:06:34 -0300

    Added missing file skiptest.h

---
+++ skiptest.h
@@ -0,0 +1,25 @@
+/*
+    This file is part of Alpertron Calculators.
+
+    Copyright 2018 Dario Alejandro Alpern
+
+    Alpertron Calculators is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Alpertron Calculators is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Alpertron Calculators.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef _SKIPTEST_H
+#define _SKIPTEST_H
+#ifdef __EMSCRIPTEN__
+void startSkipTest(void);
+void endSkipTest(void);
+#endif
+#endif
