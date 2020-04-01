3fe7f8e60294573879e00497eb3b876935590d02
alpertron <dario@alpern.com.ar>
2020-04-01 09:33:31 -0300

    Now asm.js version works again

--- lib.js
+++ lib.js
@@ -2,7 +2,33 @@ mergeInto(LibraryManager.library,
 {
   databack: function(data)
   {
-    self.postMessage(Module['Pointer_stringify'](data));
+    function PointerStringify(offset)
+{
+  var charCache = new Array(128);  // Preallocate the cache for the common single byte chars
+  var charFromCodePt = String.fromCodePoint || String.fromCharCode;
+  var result = [];
+
+  var codePt, byte1;
+
+  result.length = 0;
+
+  while (HEAPU8[offset] != 0)
+  {
+    byte1 = HEAPU8[offset++];
+    if (byte1 <= 0x7F)
+    {
+      codePt = byte1;
+    }
+    else if (byte1 <= 0xDF)
+    {
+      codePt = ((byte1 & 0x1F) << 6) | (HEAPU8[offset++] & 0x3F);
+    }
+    result.push(charCache[codePt] || (charCache[codePt] = charFromCodePt(codePt)));
+  }
+  return result.join('');
+}
+
+    self.postMessage(PointerStringify(data));
   },
   stamp: function()
   {
@@ -22,9 +48,34 @@ mergeInto(LibraryManager.library,
   },
   getCunn: function(data)
   {
+    function PointerStringify(offset)
+{
+  var charCache = new Array(128);  // Preallocate the cache for the common single byte chars
+  var charFromCodePt = String.fromCodePoint || String.fromCharCode;
+  var result = [];
+
+  var codePt, byte1;
+ 
+  result.length = 0;
+
+  while (HEAPU8[offset] != 0)
+  {
+    byte1 = HEAPU8[offset++];
+    if (byte1 <= 0x7F)
+    {
+      codePt = byte1;
+    }
+    else if (byte1 <= 0xDF)
+    {
+      codePt = ((byte1 & 0x1F) << 6) | (HEAPU8[offset++] & 0x3F);
+    }
+    result.push(charCache[codePt] || (charCache[codePt] = charFromCodePt(codePt)));
+  }
+  return result.join('');
+}
     var copyString = Module.cwrap('copyString', 'number', ['string']);
     var req = new XMLHttpRequest();
-    req.open('GET', Module['Pointer_stringify'](data), false);
+    req.open('GET', PointerStringify(data), false);
     req.send(null);
     if (req.status == 200)
     {
