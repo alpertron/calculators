da45d18ef925929ae94e8b273f9edf9ba1c65b19
Dario Alpern <dario@alpern.com.ar>
2021-04-20 09:35:03 -0300

    Fixed some warnings from Codacy

--- quintics.c
+++ quintics.c
@@ -1,21 +1,21 @@
-﻿/*
-This file is part of Alpertron Calculators.
-
-Copyright 2019 Dario Alejandro Alpern
-
-Alpertron Calculators is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Alpertron Calculators is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Alpertron Calculators.  If not, see <http://www.gnu.org/licenses/>.
-*/
+﻿//
+// This file is part of Alpertron Calculators.
+//
+// Copyright 2019-2021 Dario Alejandro Alpern
+//
+// Alpertron Calculators is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Alpertron Calculators is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Alpertron Calculators.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 // David Dummit's coefficients for solvable quintics
 // See http://www.emba.uvm.edu/~ddummit/quintics/quintics.html
