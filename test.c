38104a64ddede2ee1b518130769bb98bf1f7438c
Dario Alpern <dario@alpern.com.ar>
2021-06-05 23:25:00 -0300

    Fixed coverage

--- test.c
+++ test.c
@@ -26,7 +26,7 @@
 #include "batch.h"
 #include "polynomial.h"
 #ifndef DEBUG_CODE
-#define DEBUG_CODE 13
+#define DEBUG_CODE 16
 #endif
 void dilogText(char *baseText, char *powerText, char *modText, int groupLen);
 void gaussianText(char *valueText, int doFactorization);
@@ -297,12 +297,17 @@ int main(int argc, char* argv[])
   (void)memcpy(inputString, "6,-2,00102^1042+1""\0""2^1042+1=5^1(0)*16673^1(0)*627186185377^1(16673)*131294792925870751515684960383613518415615538737991528767912593379854404518341858118366491474959205710499826133822402120149306175263402700301^1(16673)*6864797660130609714981900799081393217269435300143305409394463459185543183397652346775704046543201000705776033378429553397612687501667381169885775070966579201^1(2)""\0\0""222""\0", 10001314 - 10000928 + 1);
   doWork();
 #elif DEBUG_CODE == 16
+  if (argc != 5)
+  {
+    printf("Enter: quadr linear const modulus\n");
+    return 1;
+  }
   quadmodText(argv[1], argv[2], argv[3], argv[4], 6);
   printf("%s\n", output);
 #elif DEBUG_CODE == 17
   if (argc != 7)
   {
-    printf("6 arguments needed\n");
+    printf("Enter 6 coefficients: x^2, xy, y^2, x, y, const.\n");
     return 1;
   }
   quadText(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6]);
