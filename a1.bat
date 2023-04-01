8895d228cee9a4ebc21d51d64b839bc0969c2ab9
Dario Alpern <dario@alpern.com.ar>
2023-04-01 11:33:16 -0300

    Fixed post-processing of files after compilation

--- a1.bat
+++ a1.bat
@@ -1,21 +1,15 @@
+copy .htaccess_orig toweb\.htaccess
+copy *00*js toweb
+del *00*js
+copy *.webmanifest toweb
+cd toweb
 perl csp.pl
 perl csp2.pl
-del *.gz
-del *.br
 echo ***** gzip *****
-cd toweb
 for %%a in (*.HTM) do gzip -9 -c %%a > %%a.gz
-cd ..
 for %%a in (*00*js) do gzip -9 -c %%a > %%a.gz
 for %%a in (*.webmanifest) do gzip -9 -c %%a > %%a.gz
 echo **** brotli ****
-cd toweb
 for %%a in (*.HTM) do brotli -f -Z %%a
-cd ..
 for %%a in (*00*js) do brotli -f -Z %%a
 for %%a in (*.webmanifest) do brotli -f -Z %%a
-copy *.webmanifest toweb
-copy *%1* toweb
-copy .htaccess toweb
-copy *.br toweb
-copy *.gz toweb
