2e73ad4a0b56e5431c01eb8d956a2c7a4ababe9e
Dario Alpern <dario@alpern.com.ar>
2024-02-16 18:32:30 -0300

    Fixed New curve and New factor in More options

--- factor.c
+++ factor.c
@@ -1819,7 +1819,7 @@ void factorExt(const BigInteger *toFactor, const int *number,
       *ptrCharFound = 0;
       diffPtrs = ptrCharFound - ptrKnownFactors;
       Dec2Bin(ptrKnownFactors, prime.limbs, (int)diffPtrs, &prime.nbrLimbs);
-      NumberLength = (int)diffPtrs;
+      NumberLength = prime.nbrLimbs;  // Set number of limbs.
       BigInteger2IntArray(pstFactors->ptrFactor, &prime);
       ptrKnownFactors = ptrCharFound + 1;
       if (getNextInteger(&ptrKnownFactors, &pstCurFactor->multiplicity, '('))
