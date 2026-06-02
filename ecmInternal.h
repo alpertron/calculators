59f760cae625cba4a0442ca0f2354094ca1c7f0e
Dario Alpern <dario@alpern.com.ar>
2026-06-02 08:32:14 -0300

    Added missing file

---
+++ ecmInternal.h
@@ -0,0 +1,3 @@
+void duplicate(limb* dupl_x, limb* dupl_z, const limb* P_x, const limb* P_z);
+void add3(limb* sum_x, limb* sum_z, const limb* Q_x, const limb* Q_z,
+  const limb* R_x, const limb* R_z, const limb* diff_x, const limb* diff_z);
